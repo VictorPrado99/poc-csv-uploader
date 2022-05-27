89aafd4b9ca084106542adeaa9c3874f5d889301
VictorPrado99 <prado.young@gmail.com>
2022-05-27 01:07:31 -0300

    wip: controller structure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/VictorPrado99/poc-csv-uploader
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,11 +1,63 @@
 package controller
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
-func SetupRoutes() {
-	http.HandleFunc("/upload", uploadCsv)
+	"github.com/gorilla/mux"
+)
+
+const (
+	GET  = "GET"
+	POST = "POST"
+	PUT  = "PUT"
+)
+
+// Method who will setup the router of this controller
+func SetupRoutes() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/upload", uploadCsv).Methods(POST)
+
+	return r
 }
 
+// Method to receive a CSV and made a async processing
 func uploadCsv(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("upload method called")
+	err := r.ParseMultipartForm(0)
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	file, handler, err := r.FormFile("myCsv")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile("temp-files", "upload-*.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
